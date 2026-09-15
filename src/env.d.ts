c4175dfb05d3edfa435778565ea0006ceb768c0b
Herob527 <szymon.wrzos@onet.pl>
2026-09-15 20:01:26 +0200

    Add user type

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ import type JobOfferService from "@core/services/JobOfferService";
 import type jwtService from "@core/services/JwtService";
 import type UserService from "@core/services/UserService";
 import type { drizzle } from "drizzle-orm/node-postgres";
+import type { User } from "./db/schema";
 
 declare global {
   interface Deps {
@@ -11,7 +12,7 @@ declare global {
     jwtService: typeof jwtService;
     userService: InstanceType<typeof UserService>;
     jobOfferService: InstanceType<typeof JobOfferService>;
-    user: Record<string, unknown> | null;
+    user: Omit<User, "password">;
   }
   namespace App {
     interface Locals extends Deps {}
